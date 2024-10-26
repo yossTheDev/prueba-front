22184a83c2d794ca40c061477ca0a9894b2a8eeb
Yoannis Sánchez Soto <yoannisgnw@gmail.com>
2024-10-26 12:42:01 -0400

    style: :lipstick: Some Improvements

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,32 +2,39 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="fixed top-4 w-full flex justify-center z-50">
-      <header className="w-[80%] bg-white shadow-md rounded">
+    <div className="sticky lg:fixed lg:top-5 top-0 w-full flex justify-center z-50">
+      <header className="w-full lg:w-[80%] bg-white lg:shadow-md lg:rounded">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
+            {/* Navegación principal */}
             <nav className="flex space-x-8">
               <Link className="text-blue-600 font-semibold" href="/">
                 INICIO
               </Link>
-              <Link className="text-blue-600 font-semibold" href="/services">
-                SERVICIOS
-              </Link>
-              <Link className="text-blue-600 font-semibold" href="/about">
-                NOSOTROS
-              </Link>
-              <Link className="text-blue-600 font-semibold" href="/tools">
-                HERRAMIENTAS
-              </Link>
-              <Link className="text-blue-600 font-semibold" href="/partners">
-                SOCIOS
-              </Link>
+
+              {/* Links adicionales ocultos en móviles */}
+              <div className="hidden lg:flex space-x-8">
+                <Link className="text-blue-600 font-semibold" href="/services">
+                  SERVICIOS
+                </Link>
+                <Link className="text-blue-600 font-semibold" href="/about">
+                  NOSOTROS
+                </Link>
+                <Link className="text-blue-600 font-semibold" href="/tools">
+                  HERRAMIENTAS
+                </Link>
+                <Link className="text-blue-600 font-semibold" href="/partners">
+                  SOCIOS
+                </Link>
+              </div>
             </nav>
+
+            {/* Botones de "Contactar" y cambio de idioma */}
             <div className="flex items-center space-x-4">
               <button className="bg-blue-600 text-white px-4 py-2 rounded-full">
                 Contactar
               </button>
-              <span className="text-gray-700">🇬🇧</span>
+              <span className="lg:inline text-gray-700">🇬🇧</span>
             </div>
           </div>
         </div>
--- src/app/sections/hero.tsx
+++ src/app/sections/hero.tsx
@@ -3,13 +3,13 @@
 export const Hero = () => {
   return (
     <>
-      <div className="bg-[#001853] h-[34rem] pt-16 lg:pt-4 grid lg:grid-cols-2 relative">
+      <div className="bg-[#001853] lg:h-[34rem] h-[36rem] pt-16 lg:pt-4 grid lg:grid-cols-2 relative">
         {/* Contenido del texto */}
-        <div className="flex flex-col items-center text-center lg:text-start lg:items-start justify-center px-8 lg:px-16">
-          <h1 className="font-extrabold text-2xl sm:text-4xl lg:text-[64px] lg:leading-[64px] text-white">
+        <div className="flex flex-col  portrait:h-fit items-center text-center lg:text-start lg:items-start justify-center px-8 lg:px-16">
+          <h1 className="font-extrabold portrait:mt-2 text-4xl sm:text-4xl lg:text-[64px] lg:leading-[64px] text-white">
             Es tiempo <br className="hidden lg:block" /> de maquetar
           </h1>
-          <p className="text-sm sm:text-base leading-6 sm:leading-[22.1px] text-neutral-300 mt-4 sm:mt-6 lg:mt-8 w-full sm:w-4/5">
+          <p className="text-xs leading-5 sm:text-base lg:leading-6 sm:leading-[22.1px] text-neutral-300 mt-4 sm:mt-6 lg:mt-8 w-full sm:w-4/5">
             Debes usar NextJS y Tailwind CSS. Cada sección de la página debe ser
             un componente individual. Usa el módulo next-intl para
             internacionalización. Usa la imaginación para desarrollar el
@@ -25,9 +25,9 @@ export const Hero = () => {
         />
 
         {/* Imagen del Hero*/}
-        <div className="order-first lg:order-none relative flex items-center justify-center lg:justify-end mb-12 lg:mb-0">
+        <div className=" order-first lg:order-none  relative flex items-center justify-center lg:justify-end lg:mb-0">
           <img
-            className="w-4/5 sm:w-[85%] md:w-3/4 lg:w-[95%] absolute bottom-0 lg:bottom-auto lg:mt-52"
+            className="w-4/5 sm:w-[85%] md:w-3/4 lg:w-[95%] lg:absolute bottom-0 lg:bottom-auto lg:mt-52"
             alt="hero image"
             src="/hero.png"
           />
